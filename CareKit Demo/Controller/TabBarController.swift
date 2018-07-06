be3844b0c8ddffb221301e85278d0944df7a5dcf
devansh.vyas <devansh.vyas@sadmins-mac-mini.local>
2018-07-06 15:34:06 +0530

    [created] Care Card and Symptoms tracker

---
+++ CareKit Demo/Controller/TabBarController.swift
@@ -0,0 +1,138 @@
+//
+//  TabBarController.swift
+//  CareKit Demo
+//
+//  Created by devansh.vyas on 06/07/18.
+//  Copyright © 2018 Solution Analysts. All rights reserved.
+//
+
+import UIKit
+import CareKit
+
+class TabBarController: UITabBarController {
+
+  //MARK: variables
+  let obj = TabBarConstants()
+  
+  lazy var carePlanStore: OCKCarePlanStore = {
+    let fileManager = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    let urlPath = fileManager[0].appendingPathComponent(obj.carePlanStorePathComponent)
+    print(urlPath)
+    if !FileManager.default.fileExists(atPath: "\(urlPath)"){
+      try! FileManager.default.createDirectory(at: urlPath, withIntermediateDirectories: true, attributes: nil)
+    }
+    return OCKCarePlanStore(persistenceDirectoryURL: urlPath)
+  }()
+
+  let activityStartDate = DateComponents(year: TabBarConstants().startYear , month: TabBarConstants().startMonth, day: TabBarConstants().startDay)
+  
+  //MARK: LifeCycle Methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let careCardVC = OCKCareCardViewController(carePlanStore: carePlanStore)
+    careCardVC.title = obj.careCardVCTitle
+    let symtomsTrackerVC = OCKSymptomTrackerViewController(carePlanStore: carePlanStore)
+    symtomsTrackerVC.title = obj.symtomsTrackerVCTitle
+    symtomsTrackerVC.delegate = self
+    viewControllers = [UINavigationController(rootViewController: careCardVC), UINavigationController(rootViewController: symtomsTrackerVC)]
+    addActivity()
+  }
+  
+  //MARK: Interventions
+  func interventions() -> [OCKCarePlanActivity]{
+    let waterSchedule = OCKCareSchedule.dailySchedule(withStartDate: activityStartDate, occurrencesPerDay: obj.waterTaskOccurrence)
+    let waterIntervention = OCKCarePlanActivity(identifier: obj.waterTaskId, groupIdentifier: nil, type: .intervention, title: obj.waterTaskTitle, text: obj.waterTaskText, tintColor: obj.waterTaskTintColor, instructions: nil, imageURL: nil, schedule: waterSchedule, resultResettable: true, userInfo: nil)
+    let exerciseSchedule = OCKCareSchedule.dailySchedule(withStartDate: activityStartDate, occurrencesPerDay: obj.exerciseTaskOccurrence)
+    let exerciseInterventions = OCKCarePlanActivity(identifier: obj.exerciseTaskId, groupIdentifier: nil, type: .intervention, title: obj.exerciseTaskTitle, text: obj.exerciseTaskText, tintColor: obj.exerciseTaskTintColor, instructions: nil, imageURL: nil, schedule: exerciseSchedule, resultResettable: true, userInfo: nil)
+    return [waterIntervention,exerciseInterventions]
+  }
+
+  //MARK: Assessments
+  func assessments() -> [OCKCarePlanActivity]{
+    let dailySchedule = OCKCareSchedule.dailySchedule(withStartDate: activityStartDate, occurrencesPerDay: obj.dailyTaskOccurrence)
+    let sleepAssessment = OCKCarePlanActivity(identifier: obj.sleepTaskId, groupIdentifier: nil, type: .assessment, title: obj.sleepTaskTitle, text: obj.sleepTaskText, tintColor: obj.sleepTaskTintColor, instructions: nil, imageURL: nil, schedule: dailySchedule, resultResettable: true, userInfo: nil)
+    let weightAssessment = OCKCarePlanActivity(identifier: obj.weightTaskId, groupIdentifier: nil, type: .assessment, title: obj.weightTaskTitle, text: obj.weightTaskText, tintColor: obj.weightTaskTintColor, instructions: nil, imageURL: nil, schedule: dailySchedule, resultResettable: true, userInfo: nil)
+    return [sleepAssessment,weightAssessment]
+  }
+
+  //MARK: adding activities
+  func addActivity(){
+    carePlanStore.activities { [unowned self](_, activities, error) in
+      if let err = error{
+        print(err.localizedDescription)
+      }
+      guard activities.count == 0 else{return}
+      
+      for activity in self.interventions() + self.assessments(){
+        self.carePlanStore.add(activity) { (_, err) in
+          guard let errors = err else {return}
+          print(errors.localizedDescription)
+        }
+      }
+    }
+  }
+  
+}
+
+
+extension TabBarController: OCKSymptomTrackerViewControllerDelegate{
+  func symptomTrackerViewController(_ viewController: OCKSymptomTrackerViewController, didSelectRowWithAssessmentEvent assessmentEvent: OCKCarePlanEvent) {
+    let alert: UIAlertController
+    if assessmentEvent.activity.identifier == obj.sleepTaskId{
+      alert = sleepAlert(event: assessmentEvent)
+    }
+    else if assessmentEvent.activity.identifier == obj.weightTaskId{
+      alert = weightAlert(event: assessmentEvent)
+    }
+    else{
+      return
+    }
+    
+    let cancelAction = UIAlertAction(title: obj.cancelAlertTitle, style: .cancel, handler: nil)
+    alert.addAction(cancelAction)
+    
+    present(alert, animated: true, completion: nil)
+  }
+  
+  //MARK: Alerts
+  func sleepAlert(event: OCKCarePlanEvent) -> UIAlertController{
+    let alert = UIAlertController(title: obj.sleepAlertTitle, message: obj.sleepAlertMessage, preferredStyle: .alert)
+    alert.addTextField { UITextField in
+      UITextField.keyboardType = .numberPad
+    }
+    
+    let doneAction = UIAlertAction(title: obj.doneActionTitle, style: .default) { _ in
+      let sleepFieldText = alert.textFields?[0].text ?? ""
+      let result = OCKCarePlanEventResult(valueString: sleepFieldText, unitString: self.obj.sleepUnit, userInfo: nil)
+      self.carePlanStore.update(event, with: result, state: .completed, completion: { (_, _, error) in
+        guard let err = error else {return}
+        print(err.localizedDescription)
+      })
+    }
+    
+    alert.addAction(doneAction)
+    
+    return alert
+  }
+  
+  func weightAlert(event: OCKCarePlanEvent) -> UIAlertController{
+    let alert = UIAlertController(title: obj.weightAlertTitle, message: obj.weightAlertMessage, preferredStyle: .alert)
+    alert.addTextField { UITextField in
+      UITextField.keyboardType = .numberPad
+    }
+    
+    let doneAction = UIAlertAction(title: obj.doneActionTitle, style: .default) { _ in
+      let weightFieldText = alert.textFields?[0].text ?? ""
+      let result = OCKCarePlanEventResult(valueString: weightFieldText, unitString: self.obj.weightUnit, userInfo: nil)
+      self.carePlanStore.update(event, with: result, state: .completed, completion: { (_, _, error) in
+        guard let err = error else {return}
+        print(err.localizedDescription)
+      })
+    }
+    
+    alert.addAction(doneAction)
+    
+    return alert
+  }
+  
+}
---
+++ CareKit Demo/Utils/Consts/Constants.swift
@@ -0,0 +1,59 @@
+//
+//  Constants.swift
+//  CareKit Demo
+//
+//  Created by devansh.vyas on 06/07/18.
+//  Copyright © 2018 Solution Analysts. All rights reserved.
+//
+
+import UIKit
+
+struct TabBarConstants {
+  
+  //MARK: Main
+  let carePlanStorePathComponent = "carePlanStore"
+  let careCardVCTitle = "Care"
+  let symtomsTrackerVCTitle = "Symtoms Tracker"
+  let startYear = 2018
+  let startMonth = 6
+  let startDay = 1
+  let dailyTaskOccurrence = UInt(1)
+  
+  //MARK: Interventions
+  let waterTaskOccurrence = UInt(8)
+  let waterTaskId = "WaterIntervention"
+  let waterTaskTitle = "Water:"
+  let waterTaskText = "You should intake atleast 8 glass of Water per Day"
+  let waterTaskTintColor = UIColor.blue
+  
+  let exerciseTaskOccurrence = UInt(2)
+  let exerciseTaskId = "ExerciseIntervention"
+  let exerciseTaskTitle = "Exercise:"
+  let exerciseTaskText = "You should do atleast 2 times exercise per Day"
+  let exerciseTaskTintColor = UIColor.cyan
+
+  //MARK: Assessment
+  let sleepTaskId = "SleepAssessment"
+  let sleepTaskTitle = "Sleep:"
+  let sleepTaskText = "Amount of sleep you take?"
+  let sleepTaskTintColor = UIColor.green
+  
+  let weightTaskId = "WeightAssessment"
+  let weightTaskTitle = "Weight:"
+  let weightTaskText = "Your Current Weight?1"
+  let weightTaskTintColor = UIColor.orange
+
+  //MARK: Alert
+  let doneActionTitle = "Done"
+  let cancelAlertTitle = "Cancel"
+  
+  let sleepAlertTitle = "Sleep"
+  let sleepAlertMessage = "Amount of sleep(in HRs):"
+  let sleepUnit = "Hrs"
+
+  let weightAlertTitle = "Weight"
+  let weightAlertMessage = "Current weight(in Kg):"
+  let weightUnit = "Kgs"
+
+}
+
